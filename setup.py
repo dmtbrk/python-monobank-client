110b37666591d8283ff56f247a42e649555d3f94
Dmytro Brykovets <dmytro.brykovets@gmail.com>
2019-07-14 14:03:18 +0300

    prepare for packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='monoback_client',
+      version='0.1.0',
+      description='Monobank API client',
+      url='https://github.com/ortymid/python-monobank-client',
+      author='Dmytro Brykovets',
+      author_email='dmytro.brykovets@gmail.com',
+      license='MIT',
+      packages='monobank_client',
+      zip_safe=False)
